33a413cad93c8e7fef42d04e8177153818917a40
soulbliss <deepak.garasangi@gmail.com>
2023-05-15 11:30:30 +0530

    add side panel colors and skeleton

---
+++ client/src/components/sidePanel/SidePanel.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SidePanel.scss";
+
+const SidePanel = () => {
+  return <div className="side-panel">I am sidePanel</div>;
+};
+
+export default SidePanel;
--- client/src/components/signUp/SignUpPage.js
+++ client/src/components/signUp/SignUpPage.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import SidePanel from "../sidePanel/SidePanel";
+import { Col, Row } from "antd";
+import "./SignUpPage.scss";
 const SignUpPage = () => {
-  return <div>I am signUpPage</div>;
+  return (
+    <div className="sign-up-page">
+      <Row className="sign-up-page-wrapper">
+        <Col className="side-panel-holder" span={8}>
+          <SidePanel />
+        </Col>
+        <Col span={8}>
+          <div>I am signUpPage</div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default SignUpPage;
