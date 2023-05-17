412c7957f21fe5f33d01118b4e0755832cacc663
soulbliss <deepak.garasangi@gmail.com>
2023-05-17 20:14:58 +0530

    Add top panel for mobile screen

---
+++ client/src/components/topPanel/TopPanel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { WisdomIconMobile } from "../icons/Icons";
+import "./TopPanel.scss";
+const TopPanel = () => {
+  return (
+    <div className="top-panel">
+      <div className="company-icon">
+        <WisdomIconMobile />
+      </div>
+    </div>
+  );
+};
+
+export default TopPanel;
