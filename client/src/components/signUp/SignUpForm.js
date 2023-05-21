7f027fe03fe3d84b6455ca20ad3fcfd482373214
soulbliss <deepak.garasangi@gmail.com>
2023-05-21 17:15:14 +0530

    update sign up form

--- client/src/components/signUp/SignUpForm.js
+++ client/src/components/signUp/SignUpForm.js
@@ -4,7 +4,7 @@ import { Button, Form, Input, Select } from "antd";
 import { register as SignUpUser } from "../../../src/api/authApi";
 import VerificationModal from "./VerificationModal";
 import { Link } from "react-router-dom";
-import { EyeOutlined, EyeInvisibleOutlined, IndianFlagIcon } from "../icons/Icons";
+import { EyeOutlined, EyeInvisibleOutlined } from "../icons/Icons";
 const { Option } = Select;
 
 /* eslint-disable no-template-curly-in-string */
@@ -30,26 +30,12 @@ const SignUpForm = () => {
     setIsModalOpen(false);
   };
   const onFormSubmit = async values => {
-    values = { ...values, name: values.firstName + " " + values.lastName };
     SignUpUser(values).then(data => {
       if (data.status === 200) {
         setIsModalOpen(true);
       }
     });
   };
-  const prefixSelector = (
-    <Form.Item name="prefix" noStyle>
-      <Select
-        style={{
-          width: 88
-        }}>
-        <Option value="91">
-          <IndianFlagIcon />
-        </Option>
-        <Option value="01">+01</Option>
-      </Select>
-    </Form.Item>
-  );
   return (
     <div className="sign-up-form">
       <div className="title">Create an account</div>
@@ -72,25 +58,14 @@ const SignUpForm = () => {
         validateMessages={validateMessages}>
         <div>
           <Form.Item
-            name="firstName"
+            name="name"
             placeholder="Please input"
             rules={[
               {
                 required: true
               }
             ]}>
-            <Input placeholder="First Name" />
-          </Form.Item>
-
-          <Form.Item
-            name="lastName"
-            placeholder="Please input"
-            rules={[
-              {
-                required: true
-              }
-            ]}>
-            <Input placeholder="Last Name" />
+            <Input placeholder="Name" />
           </Form.Item>
 
           <Form.Item
@@ -102,22 +77,18 @@ const SignUpForm = () => {
             ]}>
             <Input placeholder="Email Address" />
           </Form.Item>
+
           <Form.Item
-            name="phoneNo"
+            name="organization"
+            placeholder="Please input"
             rules={[
               {
-                required: true,
-                message: "Please input your phone number!"
+                required: true
               }
             ]}>
-            <Input
-              addonBefore={prefixSelector}
-              placeholder="Mobile Number"
-              style={{
-                width: "100%"
-              }}
-            />
+            <Input placeholder="Organization name" />
           </Form.Item>
+
           <Form.Item
             name="password"
             rules={[
